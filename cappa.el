9433707f470e02b79f74b2e30c7a58dc3b205f49
John Kitchin <jkitchin@andrew.cmu.edu>
2016-02-01 13:45:12 -0500

    updated helm sources

--- cappa.el
+++ cappa.el
@@ -31,7 +31,7 @@
 ;;; Code:
 
 (defvar cappa-repo
-  "http://catalysis-preprint-archive.github.io/cappa/preprints/"
+  "http://catalysis-preprint-archive.github.io/preprints/"
   "URL to the preprint archive.")
 
 (add-to-list 'package-archives (cons "CaPPA" cappa-repo) t)
@@ -50,54 +50,75 @@
 Returns a list of (cons label properties)."
   (let ((data (with-current-buffer
 		  (url-retrieve-synchronously
-		   "http://catalysis-preprint-archive.github.io/cappa/preprints/archive-contents")
+		   (concat cappa-repo "archive-contents"))
 		(goto-char url-http-end-of-headers)
 		(read (current-buffer)))))
     (cdr data)))
 
 
-;; This is not working. You have to do this (require 'kitchingroup-57) to get it
-;; in cappa-preprints.
+
+(defun helm-cappa-candidates ()
+  "List of CaPPA preprints that are not installed."
+  (loop for (label . properties) in (cappa-available-preprints)
+	if (not (package-installed-p label))
+	collect
+	(cons
+	 (format "%s | %s" label (elt properties 0))
+	 (cons label properties))))
+
+(defun helm-cappa-installed-candidates ()
+  (loop for (label . properties) in (cappa-available-preprints)
+	if (package-installed-p label)
+	collect
+	(cons
+	 (format "%s | %s" label (elt properties 0))
+	 (cons label properties))))
+
+
 (defvar helm-cappa-installed-preprints
   (helm-build-sync-source "CaPPA installed preprints"
-    :candidates (loop for entry in  cappa-preprints
-		      collect
-		      (cons (car entry) entry))
+    :candidates 'helm-cappa-installed-candidates
     :action (lambda (entry)
 	      (find-file
-	       (file-name-directory
-		(find-library (symbol-name (car entry)))))))
+	       (expand-file-name
+		"README.org"
+		(file-name-directory
+		 (locate-library (symbol-name 'kitchingroup-9)))))))
   "Helm source for installed preprints.")
 
 
 (defvar helm-cappa-available-preprints
   (helm-build-sync-source "CaPPA available preprints"
-    :candidates (loop for (label . properties) in (cappa-available-preprints)
-		      if (not (memq label (mapcar 'car cappa-preprints)))
-		      collect
-		      (cons label (cons label properties)))
+    :candidates 'helm-cappa-candidates
     :action (lambda (candidate)
 	      (package-install (car candidate))))
   "Helm source for preprints in CaPPA.
 TODO: remove installed packages from the list.
 Make updates apparent.")
 
-;; (defvar helm-cappa-submit-preprint
-;;   ()
-;;   "Helm source for submitting a preprint.")
+(defvar helm-cappa-submit-preprint
+  (helm-build-dummy-source "CaPPA submit preprint"
+    :filtered-candidate-transformer
+    (lambda (_candidates _source)
+      (list (or (and (not (string= helm-pattern ""))
+		     helm-pattern)
+		"Submit preprint")))
+    :action '(("Submit preprint" . (lambda (candidate)
+				     (cappa-submit-preprint)))))
+   "Helm source for submitting a preprint.")
 
 (defun cappa ()
   "Helm interface to CaPPA."
+  (interactive)
   (helm :sources '(helm-cappa-installed-preprints
 		   helm-cappa-available-preprints
-		   ;; helm-cappa-submit-preprint
-		   ))
-  )
+		   helm-cappa-submit-preprint)))
 
 
 ;; * Submit a recipe
 (defun cappa-submit-preprint ()
-  "Submit preprint to CaPPA."
+  "Submit preprint to CaPPA.
+Works for github only right now."
   (interactive)
   (let (label repo)
     ;; get the label, assume it is the directory name.
@@ -107,32 +128,67 @@ Make updates apparent.")
 
     ;; check for label.el
     (unless (file-exists-p (concat label ".el"))
-      (message "You must create %s. Doing that now.")
+      (message "You must create %s. Doing that now." (concat label ".el"))
       (find-file (concat label ".el"))
-      (auto-insert)
-      (error "Please complete this file and try again."))
+      (insert (s-format
+	       ";;; ${label}.el --- ${description}
+
+;; Copyright (C) ${year} ${name}
+
+;; Version: 0.0.1
+;; Author: ${name} <${email}>
+;; Keywords:
+;; DOI:
+;; Journal:
+;; Bibtex:
+
+;; ${license}
+
+;;; Commentary:
+
+;; Put some descriptive text here.
+
+;;; Code:
+\(require 'cappa)
+
+\(cappa-register '${label})
+
+
+\(provide '${label})
+;;; ${label}.el ends here
+"
+	       'aget
+	       `(("label" . ,label)
+		 ("description" . "CaPPA preprint for doi:")
+		 ("year" . ,(format "%s" (calendar-extract-year (calendar-current-date))))
+		 ("license" . "TBD")
+		 ("name" . ,user-full-name)
+		 ("email" . ,user-mail-address))))
+      (message-box "You need to complete %s.el before submitting." label))
 
     ;; check for README.org
     (unless (file-exists-p "README.org")
       (find-file "README.org")
-      (error "Please complete the README.org file and try again."))
+      (message-box "Please complete the README.org file before submitting."))
 
     ;; check for the repo
     (setq repo
-	  (s-trim
-	   (nth 1
-		(s-split
-		 ":"
-		 (shell-command-to-string
-		  "git config --get remote.origin.url"))))))
+	  (replace-regexp-in-string
+	   "\\.git" ""
+	   (s-trim
+	    (nth 1
+		 (s-split
+		  ":"
+		  (shell-command-to-string
+		   "git config --get remote.origin.url"))))))
 
-  ;; generate the recipe file
-  (with-temp-file "recipe"
-    (insert (format "(%s
+    ;; generate the recipe file
+    (with-temp-file label
+      (insert (format "(%s
  :fetcher github
  :repo \"%s\"
  :files (\"*\"))" label repo)))
-  (message "Send the contents of the recipe file to jkitchin@andrew.cmu.edu"))
+    (message "Send the contents of the %s file to jkitchin@andrew.cmu.edu" label)))
 
 (provide 'cappa)
 ;;; cappa.el ends here
